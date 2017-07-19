1039c176ea5dfd9b72715bfdaf3efe79d020bb23
Qi Li <liqi0425@gmail.com>
2017-07-19 15:09:08 +0100

    less local pins

--- install.sh
+++ install.sh
@@ -9,8 +9,6 @@ opam install -y depext
 opam depext -y conf-gmp.1
 
 opam pin add -n sodium https://github.com/me-box/ocaml-sodium.git#with_auth_hmac256
-opam pin add -n macaroons https://github.com/me-box/ocaml-macaroons.git
-opam pin add -n depyt https://github.com/me-box/depyt.git#fix-opam
 opam pin add -n opium https://github.com/me-box/opium.git#fix-ssl-option
 
 opam pin add -y export-service .
